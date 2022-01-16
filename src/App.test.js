9299d0acc97ac1b4f09a0cec8a8fb122d3a8f996
Mark Wlodawski <expresso@expresso.lan>
2022-01-16 18:12:55 -0500

    Write useEffect code to pass tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
 import {findByTestAtter, findByTestAttr} from '../test/testUtils';
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
+import { getSecretWord as mockGetSecretWord } from './actions';
 
 /**
  * Setup function for App component
@@ -8,7 +12,7 @@ import {findByTestAtter, findByTestAttr} from '../test/testUtils';
  */
 
 const setup = () => {
-    return shallow(<App />);
+    return mount(<App />);
 }
 
 test('renders without error', () => {
@@ -16,3 +20,23 @@ test('renders without error', () => {
     const appComponent = findByTestAttr(wrapper, 'component-app');
     expect(appComponent).toHaveLength(1);
 });
+
+describe('get secret word', () => {
+    beforeEach(() => {
+        // clear the mock calls from previous tests
+        mockGetSecretWord.mockClear();
+    })
+    test('getSecretWord on app mount', () => {
+        const wrapper = setup();
+        expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+    });
+    test('getSecretWord does not run on app update', () => {
+        const wrapper = setup();
+        mockGetSecretWord.mockClear();
+        
+
+        wrapper.setProps();
+
+        expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+    })
+})
