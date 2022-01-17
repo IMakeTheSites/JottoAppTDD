34b43ae92ca0ab95ae525fb33875760892be2993
Mark Wlodawski <expresso@expresso.lan>
2022-01-17 10:33:23 -0500

    guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,16 +2,21 @@ import axios from 'axios';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
 }
 
 /**
- * @function correctGuess
- * @returns {object} - Action object with type `CORRECT_GUESS`
+ * Returns Redux Thunk function that dispatches GUESS_WORD action and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param  {string} guessedWord 
+ * @returns {function}
  */
 
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS};
-}
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+
+    };
+};
 
 export const getSecretWord = () => {
     // TODO: write actual action in Redux
